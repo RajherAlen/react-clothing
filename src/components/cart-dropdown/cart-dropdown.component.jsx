924bb3654116d74d13faa3981e09fd68c9d83d1a
Alen <alen.rajher@gmail.com>
2020-08-26 17:39:58 +0200

    added cartIcon and cart-dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomButton from "../custom-button/custom-button";
+import "./cart-dropdown.styles.scss";
+
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown">
+      <div className="carti-item"></div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  );
+};
+
+export default CartDropdown;
